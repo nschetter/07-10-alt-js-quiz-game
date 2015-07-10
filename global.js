29282005a761de1ec641f1f9e859a67595338277
Nick <nschette@yahoo.com>
2015-07-10 11:47:53 -0500

    added given_answer function

--- global.js
+++ global.js
@@ -15,10 +15,15 @@ var questionResult = document.getElementById("question_result");
 var nextButton = document.getElementById("next");
 var totalResult = document.getElementById("total_result");
 
-//
+// runs the function process_answer_submission when user clicks submitter button
 var buttonSubmitter = document.getElementById('submitter');
 buttonSubmitter.addEventListener('click', process_answer_submission);
 
-//
+// runs the function clearAll when user clicks the next button
 var buttonNext = document.getElementById('next');
 buttonNext.addEventListener('click', clearAll);
+
+// returns the string the user entered in the input field with the id of "answer"
+function given_answer() {
+  return userAnswer.value;
+}
