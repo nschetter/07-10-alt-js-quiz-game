929ef607ea65cb3620276ebe1399656f6f645474
Nick <nschette@yahoo.com>
2015-07-10 11:43:08 -0500

    rewrote some variables

--- global.js
+++ global.js
@@ -1,14 +1,24 @@
-/**
- * Created by Samuel Stephen and Nick Schetter on 7/10/15.
- */
-
 // variables containing the divs of each question and their choices
-var divQuestion1 = getElementById('question1');
-var divQuestion2 = getElementById('question2');
-var divQuestion3 = getElementById('question3');
-var divQuestion4 = getElementById('question4');
+var divQuestion1 = document.getElementById('question1');
+var divQuestion2 = document.getElementById('question2');
+var divQuestion3 = document.getElementById('question3');
+var divQuestion4 = document.getElementById('question4');
 
 // variables containing an array of the questions AND an array of answers
 var questions = [question1, question2, question3, question4];
 var answers = ["b", "a", "d", "a"];
 
+// variables handling the submission and interactivity of the app
+var buttonSubmit = document.getElementById("submitter");
+var userAnswer = document.getElementById("answer");
+var questionResult = document.getElementById("question_result"); 
+var nextButton = document.getElementById("next");
+var totalResult = document.getElementById("total_result");
+
+//
+var buttonSubmitter = document.getElementById('submitter');
+buttonSubmitter.addEventListener('click', process_answer_submission);
+
+//
+var buttonNext = document.getElementById('next');
+buttonNext.addEventListener('click', clearAll);
