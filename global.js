80af0b70ea816da282c5ed860a75bce513f34c3b
Nick <nschette@yahoo.com>
2015-07-10 11:23:14 -0500

    renamed some variables

---
+++ global.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Samuel Stephen and Nick Schetter on 7/10/15.
+ */
+
+// variables containing the divs of each question and their choices
+var divQuestion1 = getElementById('question1');
+var divQuestion2 = getElementById('question2');
+var divQuestion3 = getElementById('question3');
+var divQuestion4 = getElementById('question4');
+
+// variables containing an array of the questions AND an array of answers
+var questions = [question1, question2, question3, question4];
+var answers = ["b", "a", "d", "a"];
+
